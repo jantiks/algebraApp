55f42e6fc27b9daaa0c55cfe7533e8ce7ba779a7
jantiks <58886410+jantiks@users.noreply.github.com>
2021-01-10 23:38:31 +0400

    Initial Commit

---
+++ algebraApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  algebraApp
+//
+//  Created by Tigran on 1/10/21.
+//  Copyright © 2021 Tigran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
